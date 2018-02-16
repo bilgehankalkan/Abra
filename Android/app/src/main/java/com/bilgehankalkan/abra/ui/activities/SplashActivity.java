1429e9b3aad8a7bfb51bfe9c115a68ed8fa28208
Bilgehan KALKAN <bilgehankalkan68@gmail.com>
2018-02-16 23:29:28 +0300

    Project created with minimum API 17 and compile API 27. SplashActivity created.

---
+++ Android/app/src/main/java/com/bilgehankalkan/abra/ui/activities/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.bilgehankalkan.abra.ui.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(this, MainActivity.class));
+    }
+}
