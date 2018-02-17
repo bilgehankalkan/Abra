5269076c5c28fcd1d850c823fa6d064b8cd59551
furkan dogan <dogan.furkan@msn.com>
2018-02-17 21:52:25 +0300

    created courier book model

---
+++ Service/src/models/book.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const collections = require("../db/collectionNames");
+const responseCode = require("../utilities/responseCode");
+const _dictionary = require("../localization/dictionary");
+const Schema = mongoose.Schema;
+const objectId = Schema.Types.ObjectId;
+
+const bookSchema = new Schema({
+    // _id: objectId,
+    userId: objectId,
+    courierId: objectId,
+    dateCreated: Date,
+    userConfirm: Boolean,
+    courierConfirm: Boolean
+}, { versionKey: false });
+
+mongoose.model("book", bookSchema, collections.book);
+var model = mongoose.model("book");
+
+model.insert = (courierId, userId,req) => {
+    var dictionary = _dictionary(req);
+    return new Promise((res, rej) => {
+        model.create({
+            userId: userId,
+            courierId: courierId,
+            dateCreated: Date.now(),
+        }).then(res)
+            .catch((err) => {
+                rej({
+                    message: dictionary.errorMEssages.systemError,
+                    statusCode: responseCode.SERVER_ERROR
+                })
+            })
+    });
+};
+
+module.exports = model;
