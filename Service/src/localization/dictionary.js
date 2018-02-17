c90248594f2858ad4c4ba2dd2580975140324f44
furkan dogan <dogan.furkan@msn.com>
2018-02-17 12:59:05 +0300

    created localization architecture
    http request header ı üzerinden (accept-language) çalışacak şekilde
    response mesajlarını veren sistem yapıldı.

---
+++ Service/src/localization/dictionary.js
@@ -0,0 +1,17 @@
+const stringFormat = require("sprintf-js").sprintf;
+const logger = require("../utilities/logger");
+const consoleLogger = logger.init(logger.types.CONSOLE);
+
+function init(req) {
+    const acceptLanguage=req.headers["Accept-Language"];
+    var dictionary = {};
+    try {
+        dictionary = require(stringFormat("./dictionary.{0}", [acceptLanguage]));
+    } catch (ex) {
+        dictionary = require('./dictionary.tr-TR.js');
+        consoleLogger.error(ex);
+    } finally {
+        return dictionary;
+    }
+}
+module.exports = init;
