e77c0aad876679341a138b4780a2f939d05d4d45
Bilgehan KALKAN <bilgehankalkan68@gmail.com>
2018-02-17 18:21:17 +0300

    Service classes edited.

--- Android/app/src/main/java/com/bilgehankalkan/abra/service/ApiInterface.java
+++ Android/app/src/main/java/com/bilgehankalkan/abra/service/ApiInterface.java
@@ -1,8 +1,24 @@
 package com.bilgehankalkan.abra.service;
 
+import com.bilgehankalkan.abra.service.models.CourierRequest;
+import com.bilgehankalkan.abra.service.models.CourierResponse;
+
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.HeaderMap;
+import retrofit2.http.POST;
+
 /**
  * Created by Bilgehan on 17.02.2018.
  */
 
 public interface ApiInterface {
+
+    String COURIER_URL = "/courier";
+
+    @POST(COURIER_URL)
+    Call<CourierResponse> postCourier(@HeaderMap Map<String, String> headerMap,
+                                      @Body CourierRequest courierRequest);
 }
