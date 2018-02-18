01d3c04e5c2140e73c8fb445c1b6cde9d9cdb3ab
Bilgehan KALKAN <bilgehankalkan68@gmail.com>
2018-02-18 13:52:18 +0300

    Splash screens theme updated. (Bitmap didn't fit properly.)

--- Android/app/src/main/java/com/bilgehankalkan/abra/ui/activities/SplashActivity.java
+++ Android/app/src/main/java/com/bilgehankalkan/abra/ui/activities/SplashActivity.java
@@ -2,15 +2,10 @@ package com.bilgehankalkan.abra.ui.activities;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
 import com.bilgehankalkan.abra.service.models.TokenRequest;
-import com.bilgehankalkan.abra.utils.messaging.FirebaseMessagingService;
 import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.FirebaseInstanceIdService;
-import com.google.firebase.messaging.FirebaseMessaging;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
@@ -23,10 +18,10 @@ public class SplashActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        sendFirebaseToken();
-
         startActivity(new Intent(this, MainActivity.class));
         finish();
+
+        sendFirebaseToken();
     }
 
     private void sendFirebaseToken() {
