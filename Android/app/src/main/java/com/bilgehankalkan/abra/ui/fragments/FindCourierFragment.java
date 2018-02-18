e010557ee7d8df324f89ff95997c588d21717f25
Bilgehan KALKAN <bilgehankalkan68@gmail.com>
2018-02-18 04:06:22 +0300

    OrderFragment updated for to list order results. FindCourierFragment is edited.

--- Android/app/src/main/java/com/bilgehankalkan/abra/ui/fragments/FindCourierFragment.java
+++ Android/app/src/main/java/com/bilgehankalkan/abra/ui/fragments/FindCourierFragment.java
@@ -2,6 +2,7 @@ package com.bilgehankalkan.abra.ui.fragments;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -13,10 +14,12 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bilgehankalkan.abra.R;
 import com.bilgehankalkan.abra.interfaces.OnSearchOptionListener;
 import com.bilgehankalkan.abra.service.models.Location;
+import com.bilgehankalkan.abra.ui.activities.FindCourierFilterActivity;
 import com.bilgehankalkan.abra.ui.activities.MainActivity;
 import com.bilgehankalkan.abra.ui.fragments.create_offer.OriginDestinationFragment;
 import com.bilgehankalkan.abra.ui.fragments.create_offer.DateTimeSelectionFragment;
@@ -38,8 +41,6 @@ public class FindCourierFragment extends BaseFragment {
     CardView cardViewSearch;
     @State String date, time, originLocationName, destinationLocationName, originLocationId, destinationLocationId;
     @State int capacity = 3;
-
-    FragNavController fragNavController;
 
     @SuppressLint("ClickableViewAccessibility")
     @Nullable
@@ -63,40 +64,27 @@ public class FindCourierFragment extends BaseFragment {
         textCapacity = rootView.findViewById(R.id.text_view_capacity_find_fragment);
         cardViewSearch = rootView.findViewById(R.id.card_view_search_find_fragment);
 
-        /*fragNavController = FragNavController.newBuilder(savedInstanceState, ((MainActivity) getActivity()).getSupportFragmentManager(), R.id.container_main)
-                //.rootFragmentListener(rootFragmentListener, 4)
-                .rootFragment(this)
-                .build();*/
-
-        fragNavController = ((MainActivity) getActivity()).fragNavController;
-
-        layoutOrigin.setOnClickListener(v -> fragNavController.pushFragment(OriginDestinationFragment.newInstance(true)));
-        layoutDestination.setOnClickListener(v -> fragNavController.pushFragment(OriginDestinationFragment.newInstance(false)));
-        layoutDate.setOnClickListener(v -> fragNavController.pushFragment(DateTimeSelectionFragment.newInstance(5)));
-        layoutTime.setOnClickListener(v -> fragNavController.pushFragment(DateTimeSelectionFragment.newInstance(2)));
         editTextOrigin.setOnTouchListener((v, event) -> {
-            fragNavController.popFragment();
-            fragNavController.pushFragment(OriginDestinationFragment.newInstance(true));,
-            return false;
+            startFilterActivity(0);
+            return true;
         });
         editTextDestination.setOnTouchListener((v, event) -> {
-            fragNavController.pushFragment(OriginDestinationFragment.newInstance(false));
+            startFilterActivity(1);
             return true;
         });
         editTextDate.setOnTouchListener((v, event) -> {
-            fragNavController.pushFragment(DateTimeSelectionFragment.newInstance(5));
-            return false;
+            startFilterActivity(2);
+            return true;
         });
         editTextTime.setOnTouchListener((v, event) -> {
-            fragNavController.pushFragment(DateTimeSelectionFragment.newInstance(2));
-            return false;
+            startFilterActivity(3);
+            return true;
         });
 
-        editTextOrigin.setText(originLocationName);
-        editTextDestination.setText(destinationLocationName);
-        editTextDate.setText(date);
-        editTextTime.setText(time);
-        textCapacity.setText(String.valueOf(capacity));
+        cardViewSearch.setOnClickListener(v -> {
+            if (checkFields())
+                startFilterActivity(4);
+        });
 
         imageMinus.setOnClickListener(v -> {
             if (Integer.valueOf(textCapacity.getText().toString()) > 1)
@@ -118,30 +106,57 @@ public class FindCourierFragment extends BaseFragment {
         return rootView;
     }
 
+    private void startFilterActivity(int mode) {
+        Intent intent = new Intent(mActivity, FindCourierFilterActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putInt("mode", mode);
+        if (mode == 4)
+            bundle.putStringArray("params", new String[]{originLocationId, destinationLocationId, date, time, "" + capacity});
+        intent.putExtras(bundle);
+        startActivity(intent);
+        mActivity.overridePendingTransition(R.anim.slide_in_up, R.anim.slide_out_up);
+    }
+
+    private boolean checkFields() {
+        if (editTextOrigin.getText().toString().length() == 0) {
+            Toast.makeText(mActivity, getString(R.string.warning_X_not_selected, getString(R.string.origin)), Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if (editTextDestination.getText().toString().length() == 0) {
+            Toast.makeText(mActivity, getString(R.string.warning_X_not_selected, getString(R.string.destination)), Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if (editTextDate.getText().toString().length() == 0) {
+            Toast.makeText(mActivity, getString(R.string.warning_X_not_selected, getString(R.string.date)), Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if (editTextTime.getText().toString().length() == 0) {
+            Toast.makeText(mActivity, getString(R.string.warning_X_not_selected, getString(R.string.time)), Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
+    }
+
     public void onOriginSelected(Location originLocation) {
         originLocationName = originLocation.getName();
         originLocationId = originLocation.getId();
-        ((MainActivity) mActivity).fragNavController.pushFragment(new FindCourierFragment());
-        mActivity.runOnUiThread(() -> editTextOrigin.setText(originLocationName));
+        editTextOrigin.setText(originLocationName);
     }
 
     public void onDestinationSelected(Location destinationLocation) {
         destinationLocationName = destinationLocation.getName();
         destinationLocationId = destinationLocation.getId();
         editTextDestination.setText(destinationLocationName);
-        //mActivity.onBackPressed();
     }
 
     public void onOriginDateSelected(String date) {
         this.date = date;
-        //editTextDate.setText(date);
-        //mActivity.onBackPressed();
+        editTextDate.setText(date);
     }
 
     public void onOriginTimeSelected(String time) {
         this.time = time;
-        //editTextDate.setText(time);
-        //mActivity.onBackPressed();
+        editTextDate.setText(time);
     }
 
     @Override
--- Android/app/src/main/java/com/bilgehankalkan/abra/ui/fragments/OrdersFragment.java
+++ Android/app/src/main/java/com/bilgehankalkan/abra/ui/fragments/OrdersFragment.java
@@ -3,11 +3,30 @@ package com.bilgehankalkan.abra.ui.fragments;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.bilgehankalkan.abra.R;
+import com.bilgehankalkan.abra.service.models.Order;
+import com.bilgehankalkan.abra.service.models.OrderListResult;
+import com.bilgehankalkan.abra.ui.activities.BaseActivity;
+import com.bilgehankalkan.abra.ui.adapters.OrderAdapter;
+import com.bilgehankalkan.abra.utils.EndlessRecyclerViewScrollListener;
+import com.bilgehankalkan.abra.utils.RecyclerItemClickListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by Bilgehan on 17.02.2018.
@@ -15,11 +34,92 @@ import com.bilgehankalkan.abra.R;
 
 public class OrdersFragment extends BaseFragment {
 
+    RecyclerView recyclerView;
+
+    OrderAdapter orderAdapter;
+    List<Order> listOrders;
+
+    public static OrdersFragment newInstance(@Nullable Bundle params) {
+        OrdersFragment ordersFragment = new OrdersFragment();
+        if (params != null)
+            ordersFragment.setArguments(params);
+        return ordersFragment;
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_orders, container, false);
 
+        recyclerView = rootView.findViewById(R.id.recycler_view_orders_fragment);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(mActivity));
+        recyclerView.addOnScrollListener(new EndlessRecyclerViewScrollListener(((LinearLayoutManager) recyclerView.getLayoutManager())) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount) {
+                if (getArguments() != null)
+                    getSearchResult(getArguments().getStringArray("params"), page);
+                else
+                    getOrders(page);
+            }
+        });
+        recyclerView.addOnItemTouchListener(new RecyclerItemClickListener(mActivity, recyclerView, new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+
+            }
+
+            @Override
+            public void onLongItemClick(View view, int position) {
+
+            }
+        }));
+
+        listOrders = new ArrayList<>();
+        orderAdapter = new OrderAdapter(mActivity, listOrders);
+        recyclerView.setAdapter(orderAdapter);
+
+        if (getArguments() != null)
+            getSearchResult(getArguments().getStringArray("params"), 0);
+        else
+            getOrders(0);
+
         return rootView;
     }
+
+    private void getSearchResult(String[] params, int pageIndex) {
+        Map<String, String> map = new HashMap<>();
+        map.put("originLocation", params[0]);
+        map.put("destinationLocation", params[1]);
+        map.put("originDate", params[2]);
+        map.put("originTime", params[3]);
+        map.put("weight", params[4]);
+        Call<OrderListResult> call = apiInterface.getSearchResult(pageIndex, 20, map);
+        call.enqueue(callback);
+    }
+
+    private void getOrders(int pageIndex) {
+        Call<OrderListResult> call = apiInterface.getOrders(BaseActivity.USER_ID, "current", pageIndex, 20);
+        call.enqueue(callback);
+    }
+
+    Callback<OrderListResult> callback = new Callback<OrderListResult>() {
+        @Override
+        public void onResponse(@NonNull Call<OrderListResult> call, @NonNull Response<OrderListResult> response) {
+            if (response.isSuccessful() && response.body() != null) {
+                OrderListResult orderListResult = response.body();
+                if (orderListResult.getCode() == 200) {
+                    listOrders.addAll(orderListResult.getOrderList());
+                    mActivity.runOnUiThread(() -> orderAdapter.notifyDataSetChanged());
+                } else
+                    Toast.makeText(mActivity, orderListResult.getMsg(), Toast.LENGTH_SHORT).show();
+            } else
+                Toast.makeText(mActivity, R.string.connection_error, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onFailure(@NonNull Call<OrderListResult> call, @NonNull Throwable t) {
+            Toast.makeText(mActivity, R.string.connection_error, Toast.LENGTH_SHORT).show();
+        }
+    };
 }
